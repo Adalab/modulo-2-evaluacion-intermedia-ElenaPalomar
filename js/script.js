0d3b71cf8d6cfc4ffef0e8a7c8a4bbce52be6213
ElenaPalomar <elenapalomarllorente@gmail.com>
2022-03-29 10:49:04 +0200

    add first steps og JS

--- js/script.js
+++ js/script.js
@@ -1,1 +1,65 @@
 'use strick';
+
+//Declarar las variables
+
+const button = document.querySelector('.js_button');
+const select = document.querySelector('.js_select');
+const change = document.querySelector('.js_change');
+
+
+
+
+
+// Declarar las funciones con una acción
+
+function getRandomNumber(max) {
+
+  return Math.ceil(Math.random() * max);
+
+}
+
+
+
+
+
+
+
+function compareValues() {
+
+  const numSelected = parseInt(select.value);
+  console.log(numSelected);
+  
+  const numAleat = getRandomNumber(6);
+  console.log(getRandomNumber(6));
+
+  if (numSelected === numAleat) {
+
+    change.innerHTML = 'Has ganado el doble de lo apostado 😁';
+
+  } else {
+
+    change.innerHTML = 'Has perdido lo apostado😭';
+  }
+
+}
+
+
+
+
+// Declarar la función manejadora
+
+function handleClickButton(event) {
+
+  event.preventDefault();
+
+  compareValues();
+
+}
+
+
+
+
+
+// Declarar el evento 'click' en el button
+
+button.addEventListener('click', handleClickButton);
