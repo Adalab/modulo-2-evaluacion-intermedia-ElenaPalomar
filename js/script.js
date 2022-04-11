baa6eeabb90b37d8cc7aa474e5fd6a745047067a
ElenaPalomar <elenapalomarllorente@gmail.com>
2022-04-11 20:35:38 +0200

    remove unnecessary code

--- js/script.js
+++ js/script.js
@@ -2,16 +2,16 @@
 
 //Declarar las variables
 
-const button = document.querySelector('.js_button');
-const select = document.querySelector('.js_select');
-const change = document.querySelector('.js_change');
+const button = document.querySelector('.js_play');
+const select = document.querySelector('.js_selected');
+const change = document.querySelector('.js_result');
 const money = document.querySelector('.js_money'); //
 const gambling = document.querySelector('.js_gambling'); //
 
 
 
 
-// Declarar las funciones con una acción
+// Funciones
 
 function getRandomNumber(max) {
 
@@ -28,22 +28,17 @@ function win() {
   const winMoney = actualMoney + betMoney*2;
 
   money.innerHTML = winMoney;
-  console.log(winMoney);
-
-  return winMoney;
 
 } 
+
 function miss() {
 
   const betMoney = parseInt(gambling.value);
   const actualMoney = parseInt(money.innerHTML);
-  
+
   const loseMoney = actualMoney - betMoney;
 
   money.innerHTML = loseMoney;
-  console.log(loseMoney);
-
-  return loseMoney;
 
 } 
 
@@ -68,7 +63,15 @@ function compareValues() {
 
 }
 
+/* function gameOver() {
 
+  if (winMoney > 200) {
+    
+
+  }
+
+}
+ */ // iba a imprimir un párrafo despues del saldo que diga, ya no puedes apostar más tanto si es meyor que 200 como si es menos que 0 y le iba a decir en ese caso (en cualquiera de los dos) me hicera un removeEventListener. Pero puede que sea mejor hacerlo con poner y quitar clases a los botones que es lo que conocemos
 
 
 // Declarar la función manejadora
